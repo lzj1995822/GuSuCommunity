ca15aaef191335692e239f0e7366488fd3095e6c
cqh <1170669705@qq.com>
2019-02-21 18:05:38 +0800

    通讯录管理

---
+++ src/views/menu/Mail.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <CommonCRUD :columns="$store.state.classInfo.properties" apiRoot="/identity/sysMail"
+                    :formColumns="formColumns">
+
+        </CommonCRUD>
+    </section>
+</template>
+
+<script>
+    import CommonCRUD from '@/components/CommonCRUD';
+    export default {
+        name: "Mail",
+        data(){
+            return {
+                formColumns: []
+            }
+        },
+
+        components: {
+            CommonCRUD
+        },
+        created() {
+            this.formColumns = this.$store.state.classInfo.properties;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
