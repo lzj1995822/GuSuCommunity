b8912e0df7b2ca6e6f79f53f9ad717ff7e9ad36b
asher <243485908@qq.com>
2019-02-26 17:25:38 +0800

    框架

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,8 @@ export default {
     text-align: center;
     color: #2c3e50;
   }
-  .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s;
+  .fade-enter-active {
+      transition: opacity 2.2s;
   }
   .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
       opacity: 0;
