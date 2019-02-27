689ea71ad0f6862b73665548e8f8bc503bd136a5
cqh <1170669705@qq.com>
2019-02-27 17:41:41 +0800

    企业管理和厂房管理

---
+++ src/views/menu/Factory.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <CommonCRUD :columns="$store.state.classInfo.properties" apiRoot="/identity/factory"
+                    :formColumns="formColumns">
+        </CommonCRUD>
+    </section>
+</template>
+
+<script>
+    import CommonCRUD from '@/components/CommonCRUD';
+    export default {
+        name: "Factory",
+        data(){
+            return {
+                formColumns: []
+            }
+        },
+
+        components: {
+            CommonCRUD
+        },
+        created() {
+            this.formColumns = this.$store.state.classInfo.properties;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
