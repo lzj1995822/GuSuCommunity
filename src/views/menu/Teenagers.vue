23c915302e028d44c71c4855d0e788edf7da5ec3
cqh <1170669705@qq.com>
2019-02-27 13:46:34 +0800

    青少年管理

--- src/views/menu/Teenagers.vue
+++ src/views/menu/Teenagers.vue
@@ -1,10 +1,28 @@
 <template>
-    
+    <section>
+        <CommonCRUD :columns="$store.state.classInfo.properties" apiRoot="/identity/teenagers"
+                    :formColumns="formColumns">
+
+        </CommonCRUD>
+    </section>
 </template>
 
 <script>
+    import CommonCRUD from '@/components/CommonCRUD';
     export default {
-        name: "Teenagers"
+        name: "Teenagers",
+        data(){
+            return {
+                formColumns: []
+            }
+        },
+
+        components: {
+            CommonCRUD
+        },
+        created() {
+            this.formColumns = this.$store.state.classInfo.properties;
+        }
     }
 </script>
 
