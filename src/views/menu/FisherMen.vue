ac79cad924f8751287bd3accb5cc1a7045ffae2e
cqh <1170669705@qq.com>
2019-02-27 15:07:55 +0800

    渔民信息

---
+++ src/views/menu/FisherMen.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <CommonCRUD :columns="$store.state.classInfo.properties" apiRoot="/identity/fisherMen"
+                    :formColumns="formColumns">
+        </CommonCRUD>
+    </section>
+</template>
+
+<script>
+    import CommonCRUD from '@/components/CommonCRUD';
+    export default {
+        name: "FisherMen",
+        data(){
+            return {
+                formColumns: []
+            }
+        },
+
+        components: {
+            CommonCRUD
+        },
+        created() {
+            this.formColumns = this.$store.state.classInfo.properties;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
