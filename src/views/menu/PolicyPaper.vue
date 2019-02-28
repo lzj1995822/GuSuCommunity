c6ab4ef8296f3b53cf1a425e37a5ad6e9357f652
cqh <1170669705@qq.com>
2019-02-28 11:23:55 +0800

    政策信息

---
+++ src/views/menu/PolicyPaper.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <CommonCRUD :columns="$store.state.classInfo.properties" apiRoot="/identity/policyPaper"
+                    :formColumns="formColumns">
+        </CommonCRUD>
+    </section>
+</template>
+
+<script>
+    import CommonCRUD from '@/components/CommonCRUD';
+    export default {
+        name: "PolicyPaper",
+        data(){
+            return {
+                formColumns: []
+            }
+        },
+
+        components: {
+            CommonCRUD
+        },
+        created() {
+            this.formColumns = this.$store.state.classInfo.properties;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
