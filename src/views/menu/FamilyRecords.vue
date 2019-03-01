816d4054d3fb0a3c20e96bc2f6ab0432b41cd0af
lxw <243485908@qq.com>
2019-03-01 12:11:47 +0800

    radio,select完善01

--- src/views/menu/FamilyRecords.vue
+++ src/views/menu/FamilyRecords.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+    import LookUp from '@/lookup';
     import CommonCRUD from '@/components/CommonCRUD';
     export default {
         name: 'FamilyRecords',
@@ -39,7 +40,29 @@
         },
         created(){
             this.formColumns = this.$store.state.classInfo.properties
-
+            this.handleSelectOptions();
+        },
+        methods: {
+            handleSelectOptions() {
+                let  items = [
+                    ['basicIncome', 'TrueFalse'],
+                    ['masterSex', 'Sex'],
+                    ['partyFamily', 'TrueFalse'],
+                    ['veteranFamily', 'TrueFalse'],
+                    ['disabledFamily', 'TrueFalse'],
+                    ['hardFamily', 'TrueFalse']
+                ].forEach(item => {
+                        this.formColumns.filter(sub => sub.name === item[0])[0].options = LookUp[item[1]]
+                    }
+                )
+                this.formColumns.forEach(item => {
+                    if(item.transferType === 'lookup') {
+                        item.formatter = (row, cell,val,index) => {
+                            return LookUp[item.lookupKey].filter(subitem => subitem.value === val)[0].label;
+                        }
+                    }
+                });
+            }
         },
         components: {
             CommonCRUD
--- src/views/menu/OrganizationStructure.vue
+++ src/views/menu/OrganizationStructure.vue
@@ -6,12 +6,30 @@
 </template>
 
 <script>
+    import LookUp from '@/lookup';
     import CommonCRUD from '@/components/CommonCRUD';
     export default {
         name: 'OrganizationStructure',
         created(){
             this.formColumns = this.$store.state.classInfo.properties
-
+            this.handleSelectOptions();
+        },
+        methods: {
+            handleSelectOptions() {
+                let  items = [
+                    ['sex', 'Sex']
+                ].forEach(item => {
+                        this.formColumns.filter(sub => sub.name === item[0])[0].options = LookUp[item[1]]
+                    }
+                )
+                this.formColumns.forEach(item => {
+                    if(item.transferType === 'lookup') {
+                        item.formatter = (row, cell,val,index) => {
+                            return LookUp[item.lookupKey].filter(subitem => subitem.value === val)[0].label;
+                        }
+                    }
+                });
+            }
         },
         components: {
             CommonCRUD
